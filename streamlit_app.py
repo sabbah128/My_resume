00f50957c9b940a590237994665617f289266ca0
sabbah128 <sabbah128@gmail.com>
2024-01-29 06:31:33 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,14 @@ from PIL import Image
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-#####################
-# Header 
-st.write('''
-# Hossein KianAra, M.Sc.
-##### *Resume* 
-''')
+
+st.write('# **Hossein KianAra, M.Sc.**')
+st.write('##### *Resume* ')
 
 image = Image.open('Myprofile.jpg')
 st.image(image, width=200)
 
-st.markdown('## Summary', unsafe_allow_html=True)
+st.markdown('## SUMMARY', unsafe_allow_html=True)
 st.info('''
 - Data scientist with expertise in machine learning and deep learning.
 - Possessing a master's degree in computer science and a bachelor's degree in statistics.
@@ -30,7 +27,7 @@ st.info('''
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #185455;">
   <a class="navbar-brand" href="https://www.linkedin.com/in/hossein-kian-ara/" target="_blank">Hossein KianAra</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -42,13 +39,12 @@ st.markdown("""
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="#work-experience">Work Experience</a>
-      </li>
+        <a class="nav-link" href="#work-experience">Work experience</a>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
-      </li>
+        <a class="nav-link" href="#achievements">Achievements</a>
+      <li class="nav-item">
+        <a class="nav-link" href="#skills">Skills</a>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
       </li>
@@ -57,8 +53,7 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
-#####################
-# Custom function for printing text
+
 def txt(a, b):
   col1, col2 = st.columns([4,1])
   with col1:
@@ -89,124 +84,66 @@ def txt4(a, b, c):
   with col3:
     st.markdown(c)
 
-#####################
-st.markdown('''
-## Education
-''')
 
-txt('**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand',
-'2002-2006')
+st.markdown('''## EDUCATION''')
+txt('**Master of degree in Computer Science.**, *Shahed, Tehran University*, IRAN', '2021-2023')
 st.markdown('''
-- GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
-- Thesis awarded `1st` Prize by the National Research Council of Thailand.
-''')
-
-txt('**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand',
-'1998-2002')
-st.markdown('''
-- GPA: `3.65`
+- GPA: `3.63`
+- Research thesis entitled `Machine learning diagnosis of active juvenile idiopathic arthritis on blood pool [99MTc] Tc-MDP scintigraphy images`.
+- Collaborated on the development of two articles with the Children's Nuclear Medicine Development Center at Dr. Gharib Hospital in Tehran.
 - Graduated with First Class Honors.
 ''')
-
-#####################
+txt('**Bachelors of Science.** (Statistics), *Azad University of Arak*, IRAN','2005-2009')
 st.markdown('''
-## Work Experience
+- Research thesis entitled `KAPPA coefficient in medical research`.
+- GPA: `3.1`
 ''')
 
-txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2011-2021')
+st.markdown('''## WORK EXPERIENCE''')
+txt('**Banking Digital Transformation** (Part-Time), Tehran, IRAN', 'Feb/2022-current')
 st.markdown('''
-- Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
-- Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
-- Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
-- Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
+- Participation in the "Digital Transformation of Maskan Bank" initiative, which aims to incorporate AI and data science to evaluate customers.
+            (The project has a statistical population of over 9 million people.)
+- Achieved a 15% increase in online transactions and digital banking usage through the implementation of new digital channels and features.
+''')
+txt('**Customer Credit Analyst** (Full-Time), Arak, IRAN', 'Jan/2017-current')
+st.markdown('''
+- Reduced risk through analyzing financial data and credit reports for accurate credit assessments.
+- Redesigned and implemented a customer validation algorithm, resulting in a twofold increase in validation speed.
+- Developed a clustering algorithm in Python to identify influential customers in branch credit using a dataset of over 9,000 data points.
+''')
+txt('**Senior Banking Affairs Officer** (Full-Time), Arak, IRAN', 'Nov/2013-Jan/2017')
+st.markdown('''
+- Directed a team of seven individuals to reduce customer claims, resulting in a 30% reduction in loan delinquencies.
+- Collaborated with underwriters and loan officers to close 40+ loans per month.
+- Trained and mentored 10+ junior colleagues, resulting in improved team performance and productivity.
+''')
+txt('**Junior Banking Affairs Officer** (Full-Time), Arak, IRAN', 'Oct/2011-Nov/2013')
+st.markdown('''
+- Planned, executed, and optimized digital marketing campaigns, resulting in a 15% increase in customer engagement with online banking services.
+- Implemented customer forecasting models, resulting in a 10% improvement in demand forecast accuracy.
+- Oversaw the processing of more than 3,000 transactions on a monthly basis.
 ''')
 
-txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2012-2021')
-txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2009-2012')
-txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2006-2009')
+
+st.markdown('''## ACHIEVEMENTS''')
 st.markdown('''
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
+- Ranked first in the master's program at the Faculty of Computer Sciences.
+- Secured first place in the youth category programming competition in Markazi province and ranked fourth nationally in Iran.
+- Published two publications in partnership with the Dr. Gharib Hospital's Children's Nuclear Medicine Development Centre in Tehran.
 ''')
 
-txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
-'2016')
-st.markdown('''
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
-''')
-
-txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
-'2019-Present')
-st.markdown('''
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
-''')
-
-txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
-'2019-Present')
-st.markdown('''
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-''')
-
-txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
-'2019-Present')
-st.markdown('''
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
-''')
-
-#####################
-st.markdown('''
-## Bioinformatics Tools
-''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
-txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
-txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
-txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
-txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
-txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
-txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
-txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
-txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
-txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
-txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
-txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
-txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
-
-
-#####################
-st.markdown('''
-## Skills
-''')
-txt3('Programming', '`Python`, `R`, `Linux`')
-txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
-txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
-txt3('Machine Learning', '`scikit-learn`')
+st.markdown('''## SKILLS''')
+txt3('Programming', '`Python`')
+txt3('Data Preparation/Analysis', '`SQL`, `Excel`, `Pandas`, `Numpy`')
+txt3('Data Visualization', '`Power BI`,`Matplotlib`, `Seaborn`, `Bokeh`')
+txt3('Sql & noSql db', '`MySQL`, `Neo4j`')
+txt3('Machine Learning', '`Scikit-learn`, `Pycaret`')
 txt3('Deep Learning', '`TensorFlow`')
-txt3('Web development', '`Flask`, `HTML`, `CSS`')
-txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+txt3('Model deployment', '`Streamlit`')
 
-#####################
-st.markdown('''
-## Social Media
-''')
+
+st.markdown('''## Social Media''')
 txt2('LinkedIn', 'https://www.linkedin.com/in/chanin-nantasenamat')
 txt2('Twitter', 'https://twitter.com/thedataprof')
 txt2('GitHub', 'https://github.com/chaninn/')
