2159bea5cc15df82f21bddc4057d43045ba6977e
sabbah128 <sabbah128@gmail.com>
2024-01-29 06:58:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.info('''
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #326e64;">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3c6e6e;">
   <a class="navbar-brand" href="https://www.linkedin.com/in/hossein-kian-ara/" target="_blank">Hossein KianAra</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
